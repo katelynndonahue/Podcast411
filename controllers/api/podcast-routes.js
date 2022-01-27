6e7cf2d0f4cb9e7692995ea402b769bd1db587be
MaxStump13 <stumpmaximilian@gmail.com>
2022-01-27 14:29:07 -0500

    added withAuth and podcast route

--- controllers/api/podcast-routes.js
+++ controllers/api/podcast-routes.js
@@ -1,2 +1,42 @@
 const router = require("express").Router();
-module.exports=router
+const router = require('express').Router();
+const { Podcast } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPodcast = await Podcast.create({
+      ...req.body,
+      playlist_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPodcast);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const podcastData = await Podcast.destroy({
+      where: {
+        id: req.params.id,
+        playlist_id: req.session.playlist_id,
+      },
+    });
+
+    if (!podcastData) {
+      res.status(404).json({ message: 'No podcast found with this id!' });
+      return;
+    }
+
+    res.status(200).json(podcastData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
