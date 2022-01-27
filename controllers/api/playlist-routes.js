eb917d617ca6c8612a82afdec9c77a07da83217c
Rayman244 <rayhewitt42@icloud.com>
2022-01-27 14:32:16 -0500

    added additional routes

--- controllers/api/playlist-routes.js
+++ controllers/api/playlist-routes.js
@@ -1,3 +1,94 @@
 const router = require("express").Router();
+const {User,Podcast,Playlist}= require('../../models')
 
-module.exports=router;
+
+// getAll
+router.get('/', async (req,res)=>{
+    try {
+        const playlistData = await Playlist.findAll({
+            include:[{model:User},{model:Podcast}]
+        })
+        res.status(200).json(playlistData)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+})
+
+// READ
+router.get("/:id", async (req, res) => {
+    // find user by its id
+    try {
+      const playlistData = await Playlist.findByPk(req.params.id, {
+        include: [{model:User},{model:Podcast}],
+      });
+      res.status(200).json(playlistData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// CREATE
+router.post("/", async (req, res) => {
+    // create a new user
+    try {
+      const playlistData = await Playlist.create(req.body);
+      if (!playlistData) {
+        res.status(404).json({ message: "Please try again!" });
+        return;
+      }
+      res.status(200).json(playlistData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+ //   UPDATE
+ router.put("/:id", async (req, res) => {
+    // update a category by its `id` value
+    try {
+      const playlistData = await Playlist.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if(!playlistData){
+        res.status(404).json({ message: "Please try again!" });
+        return;
+      }
+    return res.status(200).json(playlistData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    
+  });
+  router.delete("/:id", async (req, res) => {
+    // delete a blog
+    try {
+      const playlistData = await Playlist.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if(!playlistData){
+        res.status(404).json({ message: "Please enter a valad Playlist!" });
+        return;
+      }
+    return res.status(200).json(playlistData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+
+
+
+
+
+
+module.exports=router
+
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,5 +48,61 @@ router.post("/", async (req, res) => {
       res.status(500).json(err);
     }
   });
+// Login
+router.post('/login', async (req, res) => {
+    try {
+      const dbUserData = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (!dbUserData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      const validPassword = await dbUserData.checkPassword(req.body.hashedPassword);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+//   DELETE
+  router.delete("/:id", async (req, res) => {
+    // delete a user
+    try {
+      const userData = await User.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if(!userData){
+        res.status(404).json({ message: "Please enter a valad Category!" });
+        return;
+      }
+    return res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 
-  module.exports= router;
+module.exports= router
+
