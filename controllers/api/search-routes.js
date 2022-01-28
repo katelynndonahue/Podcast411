a4a2b185295ef76b8050970a7b6ba9818ad70171
MaxStump13 <stumpmaximilian@gmail.com>
2022-01-28 14:20:53 -0500

    prep for pull

--- controllers/api/search-routes.js
+++ controllers/api/search-routes.js
@@ -2,6 +2,15 @@ const router = require('require').Router();
 // const client = require('../../config/ListenNotes')
 const {Client} =require('podcast-api');
 const client = Client({apiKey: process.env.API_KEY});
+let searchEl = document.querySelector(".search");
+searchEl.value.trim();
+router.get('/', async (req,res) =>{
+    client.search()
+})
 client.search({
+    q: searchEl,
+    episode_count_max: 25,
+    safe_mode: 1
+
     
 })
