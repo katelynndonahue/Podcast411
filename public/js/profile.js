4dd95971657290161d0eed60c1be525612a77879
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-01-31 14:15:34 -0500

    slick implementation

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,15 +1,5 @@
-// On swipe event
-$('slider').on('swipe', function(event, slick, direction){
-    console.log(direction);
-    // left
-  });
-  
-  // On edge hit
-  $('.slider').on('edge', function(event, slick, direction){
-    console.log('edge was hit')
-  });
-  
-  // On before slide change
-  $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    console.log(nextSlide);
+  $('.carousel').slick({
+  slidesToShow: 3,
+  dots:true,
+  centerMode: true,
   });
