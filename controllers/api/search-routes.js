27c8e6625369ac151645e609bc4518be883585bb
MaxStump13 <stumpmaximilian@gmail.com>
2022-01-30 20:32:18 -0500

    ready for commit

--- controllers/api/search-routes.js
+++ controllers/api/search-routes.js
@@ -1,28 +1,35 @@
 const router = require('express').Router();
 // const client = require('../../config/ListenNotes')
 const {Client} =require('podcast-api');
+const withAuth = require('../../utils/auth');
 const client = Client({apiKey: process.env.API_KEY});
-let searchEl = document.querySelector(".search");
-searchEl.value.trim();
-router.get('/', withAuth, async (req,res) =>{
+// let searchEl = document.querySelector(".search");
+// searchEl.value.trim();
+router.get('/', async (req,res) =>{
     client
 			.search({
-				q: searchEl,
+				q: "hello",
 				episode_count_max: 25,
 				safe_mode: 1,
 			})
 			.then((response) => {
 				const podcastData = response.data.results;
 				console.log(podcastData);
-			});
+                podcasts = podcastData.map((pod)=>pod)
+                res.render('search',
+                podcasts)
+			}).catch((error)=>{
+                console.log(error);
+            })
 })
 // client.search({
-//     q: searchEl,
+//     q: "hello",
 //     episode_count_max: 25,
 //     safe_mode: 1   
 // }).then((response)=>{
 //     const podcastData = response.data.results;
 //     console.log(podcastData);
+
 // })
 
 module.exports = router;
